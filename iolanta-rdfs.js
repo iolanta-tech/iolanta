a0a536f7f5c8e1a0dd329227f7d829018fe3f0a6
Anatoly Scherbakov <altaisoft@gmail.com>
2020-08-04 17:15:24 +0700

    slim.js repeat

---
+++ iolanta-rdfs.js
@@ -0,0 +1,27 @@
+import {Slim} from './lib/Slim.js';
+import './lib/directives/repeat.js';
+
+Slim.tag(
+    'iolanta-rdfs-entity-cards',
+    `
+      <div class="ui four cards">
+        <div class="ui card" s:repeat="item in items" bind>
+          <div class="content">
+            <div class="header">{{ item.label }}</div>
+            <div class="meta">
+              <span class="category">
+                subclass of <strong>{{ item.superclass.label }}</strong>
+              </span>
+            </div>
+            <div class="description">{{ item.comment }}</div>
+          </div>
+          <div class="extra content">
+            <small class="right floated">{{ item.prefixed_id }}</small>
+          </div>
+        </div>
+      </div>
+    `,
+    class IolantaRDFSEntityCard extends Slim {
+        
+    }
+)
