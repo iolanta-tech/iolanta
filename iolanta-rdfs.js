da2fcc9f7cde9b327f9cd5e98c396b9c6bd1066c
Anatoly Scherbakov <altaisoft@gmail.com>
2020-08-08 22:37:24 +0700

    Working but dummy front end

--- iolanta-rdfs.js
+++ iolanta-rdfs.js
@@ -5,23 +5,32 @@ Slim.tag(
     'iolanta-rdfs-entity-cards',
     `
       <div class="ui four cards">
-        <div class="ui card" s:repeat="item in items" bind>
+        <div class="ui card" s:repeat="items as item" bind>
           <div class="content">
-            <div class="header">{{ item.label }}</div>
+            <div class="header">{{item.label}}</div>
             <div class="meta">
               <span class="category">
-                subclass of <strong>{{ item.superclass.label }}</strong>
+                subclass of <strong>{{item.superclass.label}}</strong>
               </span>
             </div>
-            <div class="description">{{ item.comment }}</div>
+            <div class="description">{{item.comment}}</div>
           </div>
           <div class="extra content">
-            <small class="right floated">{{ item.prefixed_id }}</small>
+            <small class="right floated">{{item.prefixed_id}}</small>
           </div>
         </div>
       </div>
     `,
     class IolantaRDFSEntityCard extends Slim {
-        
+        onBeforeCreated() {
+            this.items = [{
+                label: 'foo',
+                superclass: {
+                    label: 'boo',
+                },
+                comment: 'bar',
+                prefixed_id: 'baz',
+            }]
+        }
     }
 )
