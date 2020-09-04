8e4a27e1253f4308782480b2d1cd0a4482ea2b63
Anatoly Scherbakov <altaisoft@gmail.com>
2020-09-04 22:52:10 +0700

    #5 Garbage code removed

--- iolanta/app.py
+++ iolanta/app.py
@@ -1,7 +1,6 @@
-import functools
 import json
 from pathlib import Path
-from typing import List, Optional
+from typing import Optional
 
 import rdflib
 import requests
@@ -10,30 +9,13 @@ from fastapi.staticfiles import StaticFiles  # type: ignore
 from pydantic import AnyUrl
 from pyld import jsonld
 from rdflib import ConjunctiveGraph
-from rdflib.plugins.memory import Memory, IOMemory
+from rdflib.plugins.memory import IOMemory
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 from iolanta import models
-from iolanta.models import Query, Lens
 
 STATIC_DIRECTORY = Path(__file__).parent.parent / 'static'
-
-
-def some_function(first: int, second: int) -> int:
-    """
-    We use this function as an example for some real logic.
-
-    This is how you can write a doctest:
-
-    .. code:: python
-
-        >>> some_function(2, 3)
-        5
-
-    Enjoy!
-    """
-    return first + second
 
 
 app = FastAPI()
@@ -64,77 +46,6 @@ def graph():
     iolanta_rdfs_graph.parse(str(STATIC_DIRECTORY / 'iolanta-rdfs.n3'), format='n3')
 
     return universe
-
-
-def query_graph(query: Query) -> dict:    # type: ignore
-    triples = list(graph().query(query.query))
-    response_graph = rdflib.Graph()
-
-    for triple in triples:
-        response_graph.add(triple)
-
-    context = query.context
-
-    jsonld_data = json.loads(response_graph.serialize(
-        format='json-ld',
-        context=context,
-    ))
-
-    return jsonld_data
-
-
-@app.post('/sparql')
-def sparql(query: Query):
-    """Execute a SPARQL query via a POST request."""
-    return query_graph(query=query)
-
-
-def get_lens(url: str) -> Lens:
-    with open(STATIC_DIRECTORY / 'lens.sparql', 'r') as lens_query_file:
-        lens_query = lens_query_file.read()
-
-    lens_query = lens_query.replace('$iri', url)
-
-    g = graph()
-    lenses = list(g.query(lens_query))
-    (sparql_path_ref, frame_path_ref), *etc = lenses
-
-    # TODO we automatically assume this is a URIRef, but it can be just text
-    sparql_text = requests.get(str(sparql_path_ref)).text
-    frame_content = requests.get(str(frame_path_ref)).json()
-
-    return Lens(
-        sparql_text=sparql_text,
-        frame=frame_content,
-    )
-
-
-def execute_lens(iri: str, via: str) -> dict:  # type: ignore
-    g = graph()
-    lens = get_lens(url=via)
-
-    subgraph = g.query(lens.sparql_text)
-
-    # TODO can serialize() return a dict?
-    jsonld_subgraph = json.loads(subgraph.serialize(
-        format='json-ld',
-        context={
-            '@vocab': 'http://www.w3.org/2000/01/rdf-schema#',
-        }
-    ))
-
-    jsonld_subgraph = jsonld.frame(jsonld_subgraph, lens.frame)
-
-    return jsonld_subgraph
-
-
-@app.get('/query')
-def query(iri: AnyUrl, via: AnyUrl):
-    """Fetch given datum from Semantic Web and represent it via given Lens."""
-    return execute_lens(
-        iri=iri,
-        via=via,
-    )
 
 
 LENS_QUERY = '''
