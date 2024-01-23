9a7cc78309a9c2f7aedd0500f9465458e355bcc1
Anatoly Scherbakov <altaisoft@gmail.com>
2024-01-24 00:01:52 +0400

    Start splitting default facet logic using `compose()`

--- iolanta/facets/textual_default/facets.py
+++ iolanta/facets/textual_default/facets.py
@@ -1,10 +1,14 @@
 import logging
 import sys
 import traceback
+from functools import cached_property
 from pathlib import Path
+from typing import Iterable
 
 import funcy
-from rdflib import URIRef
+import rdflib
+from rdflib import URIRef, DC, SDO, RDFS
+from rdflib.term import Node, Literal, BNode
 from rich.markdown import Markdown
 from textual.app import ComposeResult
 from textual.containers import Vertical
@@ -17,10 +21,108 @@ from iolanta.models import ComputedQName, NotLiteralNode
 from iolanta.namespaces import IOLANTA
 
 
+class Title(Label):
+    """Page title."""
+
+    CSS_PATH = 'tcss/title.fucktcss'  # FIXME does not seem to work
+
+
 class TextualDefaultFacet(Facet[Widget]):
     """Default rendering engine."""
 
+    @cached_property
+    def grouped_properties(self) -> dict[NotLiteralNode, list[Node]]:
+        """Properties of current node & their values."""
+        property_rows = self.stored_query(
+            'properties.sparql',
+            iri=self.iri,
+        )
+
+        property_pairs = [
+            (row['property'], row['object'])
+            for row in property_rows
+        ]
+
+        return funcy.group_values(property_pairs)
+
+    @property
+    def title(self) -> str:
+        """
+        Candidates for the page title.
+
+        FIXME: Here, we mutate `grouped_properties` :(
+
+        TODO: Implement locale support.
+        """
+        choices = [
+            title
+            for title_property in [DC.title, SDO.title, RDFS.label]
+            for title in self.grouped_properties.pop(title_property, [])
+            if isinstance(title, Literal)
+        ]
+
+        if choices:
+            return choices[0].value
+
+        return self.format_clickable_link(self.iri)
+
+    def format_clickable_link(self, iri: NotLiteralNode):
+        """Format given IRI as clickable link, if possible."""
+        if isinstance(iri, BNode):
+            return str(iri)
+
+        qname: ComputedQName | NotLiteralNode = node_to_qname(
+            iri,
+            self.iolanta.graph,
+        )
+        if isinstance(qname, ComputedQName):
+            namespace_part = (
+                f"[@click=goto('{qname.namespace_url}')]{qname.namespace_name}[/]"
+            )
+            term_part = iri if iri == self.iri else (
+                f"[@click=goto('{iri}')]{qname.term}[/]"
+            )
+
+            return f'{namespace_part}:{term_part}'
+
+        if iri == self.iri:
+            return str(iri)
+
+        return f"[@click=goto('{iri}')]{iri}[/]"
+
+    @cached_property
+    def description(self) -> str | None:
+        """
+        Candidates for description.
+
+        FIXME: We mutate `grouped_properties` here.
+        """
+        choices = [
+            description
+            for description_property in [
+                DC.description,
+                SDO.description,
+                RDFS.comment,
+            ]
+            for description in self.grouped_properties.pop(description_property, [])
+            if isinstance(description, Literal)
+        ]
+
+        try:
+            return choices[0].value
+        except IndexError:
+            return None
+
+    def compose(self) -> Iterable[Widget]:
+        """Compose widgets."""
+        yield Title(f'[bold white]{self.title}[/bold white]')
+
+        if self.description:
+            yield Label(self.description)
+
     def show(self) -> Widget:
+        return Vertical(*self.compose())
+
         rows = self.stored_query('label.sparql', iri=self.iri)
         first_row = funcy.first(rows)
 
@@ -40,15 +142,7 @@ class TextualDefaultFacet(Facet[Widget]):
             label=label,
             comment=comment or '',
         )
-        property_rows = self.stored_query(
-            'properties.sparql',
-            iri=self.iri,
-        )
 
-        property_pairs = [
-            (row['property'], row['object'])
-            for row in property_rows
-        ]
 
         children = [Label(Markdown(text))]
 
@@ -57,7 +151,7 @@ class TextualDefaultFacet(Facet[Widget]):
             self.stored_query('instances.sparql', iri=self.iri),
         )
         if instances:
-            children.append(Label('\n[bold]A few instances of this class[/]\n'))
+            # Label('\n[bold]A few instances of this class[/]\n')
             children.append(Label(
                 ' · '.join(
                     self.render(
@@ -101,8 +195,7 @@ class TextualDefaultFacet(Facet[Widget]):
 
             children.append(nodes_table)
 
-        grouped_properties = funcy.group_values(property_pairs)
-        if grouped_properties:
+        if self.grouped_properties:
             properties_table = DataTable(show_header=True, show_cursor=False)
             properties_table.add_columns('Property', 'Value')
             properties_table.add_rows([
