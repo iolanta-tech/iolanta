ddb6437c68bab765b7ab880b981f5664fb2e2b1f
Anatoly Scherbakov <altaisoft@gmail.com>
2024-01-28 23:54:13 +0400

    Display FOAF ontology terms, even if dirty

--- iolanta/facets/textual_ontology/facets.py
+++ iolanta/facets/textual_ontology/facets.py
@@ -1,9 +1,45 @@
+import funcy
+from rdflib import URIRef, Graph
+from textual.containers import Vertical
 from textual.widget import Widget
-from textual.widgets import Label
+from textual.widgets import Label, Static, Button
 
 from iolanta.facets.facet import Facet, FacetOutput
 
 
+class OntologyTerms(Widget):
+    DEFAULT_CSS = """
+    OntologyTerms {
+        layout: grid;
+        grid-size: 3;
+        grid-rows: 3;
+    }
+    
+    .box {
+        width: 100%;
+        # border: none;
+        # background: none;
+    }
+    """
+
+
 class OntologyFacet(Facet[Widget]):
     def show(self) -> Widget:
-        return Label('I AM AN ONTOLOGY')
+        terms = funcy.lpluck(
+            'term',
+            self.stored_query('terms.sparql', iri=self.iri),
+        )
+
+        # g: Graph = self.iolanta.graph
+        # raise ValueError([k for k, v in g.namespaces()])
+
+        return OntologyTerms(*[
+            Button(
+                self.render(
+                    term,
+                    environments=[URIRef('https://iolanta.tech/env/title')],
+                ),
+                classes='box',
+            )
+            for term in terms
+        ])
