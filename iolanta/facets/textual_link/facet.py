21b5782008398fbbbb5bdba125bd72485f9b9301
Anatoly Scherbakov <altaisoft@gmail.com>
2024-08-11 01:15:56 +0400

    Render `Literal` as gray text

--- iolanta/facets/textual_link/facet.py
+++ iolanta/facets/textual_link/facet.py
@@ -1,4 +1,6 @@
-from rdflib import URIRef
+from rdflib import Literal, URIRef
+from rich.style import Style
+from rich.text import Text
 
 from iolanta.cli.formatters.node_to_qname import node_to_qname
 from iolanta.facets.facet import Facet, FacetOutput
@@ -6,10 +8,13 @@ from iolanta.models import ComputedQName, NotLiteralNode
 
 
 class TextualLinkFacet(Facet[str]):
-    def show(self) -> str:
+    def show(self) -> str | Text:
+        if isinstance(self.iri, Literal):
+            return Text(self.iri, style=Style(color='grey37'))
+
         label = self.render(
             self.iri,
-            environments=[URIRef('https://iolanta.tech/env/title')]
+            environments=[URIRef('https://iolanta.tech/env/title')],
         )
 
         return f'[@click="app.goto(\'{self.iri}\')"]{label}[/]'
