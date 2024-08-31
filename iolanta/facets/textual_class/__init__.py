6f90d0930afbcf31fb2aceae4196fbfc5c0eb9a6
Anatoly Scherbakov <altaisoft@gmail.com>
2024-08-31 18:09:09 +0400

    Use single quotes for `__all__` where linters are unhappy

--- iolanta/facets/textual_class/__init__.py
+++ iolanta/facets/textual_class/__init__.py
@@ -1,3 +1,3 @@
 from iolanta.facets.textual_class.facets import Class
 
-__all__ = ["Class"]
+__all__ = ['Class']
--- iolanta/facets/textual_provenance/__init__.py
+++ iolanta/facets/textual_provenance/__init__.py
@@ -1,3 +1,3 @@
 from iolanta.facets.textual_provenance.facets import TextualProvenanceFacet
 
-__all__ = ["TextualProvenanceFacet"]
+__all__ = ['TextualProvenanceFacet']
