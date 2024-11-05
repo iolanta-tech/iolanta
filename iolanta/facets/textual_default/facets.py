1206eea4331f9c54a1616a920d191a2a87bee827
Anatoly Scherbakov <altaisoft@gmail.com>
2024-11-05 12:19:15 +0400

    Remove redundant `Properties` tab & some dead code

--- iolanta/facets/textual_default/facets.py
+++ iolanta/facets/textual_default/facets.py
@@ -1,20 +1,17 @@
 import functools
-from typing import Iterable
 from xml.dom import minidom  # noqa: S408
 
 import funcy
-from rdflib import DC, RDFS, SDO, URIRef
-from rdflib.term import BNode, Literal, Node
+from rdflib import DC, RDFS, SDO
+from rdflib.term import Literal, Node
 from rich.syntax import Syntax
+from textual.containers import Vertical
 from textual.widget import Widget
-from textual.widgets import Label, Static
+from textual.widgets import Static
 
-from iolanta.cli.formatters.node_to_qname import node_to_qname
-from iolanta.facets.errors import FacetNotFound
 from iolanta.facets.facet import Facet
 from iolanta.facets.page_title import PageTitle
 from iolanta.facets.textual_default.widgets import (
-    ContentArea,
     LiteralPropertyValue,
     PropertiesContainer,
     PropertyName,
@@ -22,7 +19,7 @@ from iolanta.facets.textual_default.widgets import (
     PropertyValue,
     PropertyValues,
 )
-from iolanta.models import ComputedQName, NotLiteralNode
+from iolanta.models import NotLiteralNode
 
 
 class TextualDefaultFacet(Facet[Widget]):   # noqa: WPS214
@@ -79,51 +76,6 @@ class TextualDefaultFacet(Facet[Widget]):   # noqa: WPS214
                 PropertyValues(*property_values),
             )
 
-    @property
-    def title(self) -> str:
-        """
-        Candidates for the page title.
-
-        FIXME: Here, we mutate `grouped_properties` :(
-
-        TODO: Implement locale support.
-        """
-        choices = [
-            title
-            for title_property in [DC.title, SDO.title, RDFS.label]
-            for title in self.grouped_properties.pop(title_property, [])
-            if isinstance(title, Literal)
-        ]
-
-        if choices:
-            return choices[0].value
-
-        return self.format_clickable_link(self.iri)
-
-    def format_clickable_link(self, iri: NotLiteralNode):
-        """Format given IRI as clickable link, if possible."""
-        if isinstance(iri, BNode):
-            return str(iri)
-
-        qname: ComputedQName | NotLiteralNode = node_to_qname(
-            iri,
-            self.iolanta.graph,
-        )
-        if isinstance(qname, ComputedQName):
-            namespace_part = (
-                f"[@click=app.goto('{qname.namespace_url}')]{qname.namespace_name}[/]"
-            )
-            term_part = qname.term if iri == self.iri else (
-                f"[@click=app.goto('{iri}')]{qname.term}[/]"
-            )
-
-            return f'{namespace_part}:{term_part}'
-
-        if iri == self.iri:
-            return str(iri)
-
-        return f"[@click=app.goto('{iri}')]{iri}[/]"
-
     @functools.cached_property
     def description(self) -> str | Syntax | None:
         """
@@ -174,51 +126,10 @@ class TextualDefaultFacet(Facet[Widget]):   # noqa: WPS214
 
         return PropertiesContainer(*self.rows)
 
-    def compose(self) -> Iterable[Widget]:
-        """Compose widgets."""
-        yield PageTitle(self.iri)
-
-        if self.description:
-            yield Label(self.description, id='description')
-
-    @property
-    def instances(self):
-        """Instances of this class."""
-        return self.render(
-            self.iri,
-            as_datatype=URIRef('https://iolanta.tech/cli/default/instances'),
-        )
-
-    @property
-    def terms(self):
-        """Terms of this ontology."""
-        return self.render(
-            self.iri,
-            as_datatype=URIRef('https://iolanta.tech/cli/default/terms'),
-        )
-
-    @property
-    @funcy.post_processing(dict)
-    def tabs(self):
-        """
-        Tabs available for this IRI.
-
-        TODO: Make this dynamic with `.render_all()`.
-        """
-        try:
-            yield 'Instances', self.instances
-        except FacetNotFound:
-            ...   # noqa: WPS428
-
-        try:
-            yield 'Terms', self.terms
-        except FacetNotFound:
-            ...   # noqa: WPS428
-
-        yield 'Properties', self.properties
-
     def show(self) -> Widget:
         """Render the content."""
-        area = ContentArea(*self.compose())
-        area.tabs = self.tabs
-        return area
+        return Vertical(
+            PageTitle(self.iri, extra='& its RDF properties'),
+            Static(self.description or ''),
+            self.properties,
+        )
