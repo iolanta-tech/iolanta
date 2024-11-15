d02cfc2edf432577b7732e6629dfe64c9b6b36c8
Anatoly Scherbakov <altaisoft@gmail.com>
2024-11-15 16:42:39 +0400

    Fix that

--- iolanta/cli/formatters/choose.py
+++ iolanta/cli/formatters/choose.py
@@ -4,9 +4,9 @@ from rdflib import Graph
 
 from iolanta.cli.formatters.csv import csv_print
 from iolanta.cli.formatters.json import print_json
-from iolanta.node_to_qname import node_to_qname
 from iolanta.cli.formatters.pretty import pretty_print
 from iolanta.models import QueryResultsFormat
+from iolanta.node_to_qname import node_to_qname
 from ldflex.ldflex import SelectResult
 
 
