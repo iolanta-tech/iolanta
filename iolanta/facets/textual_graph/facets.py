f6e6d3e971e99b4be739485ac01f9c2b41585b33
Anatoly Scherbakov <altaisoft@gmail.com>
2024-12-27 12:11:19 +0400

    #190 Render titles of nodes in the Triples viwe

--- iolanta/facets/textual_graph/facets.py
+++ iolanta/facets/textual_graph/facets.py
@@ -1,34 +1,117 @@
 import functools
+from dataclasses import dataclass
 
 from mypy.memprofile import defaultdict
+from rdflib.term import Literal, Node
+from rich.text import Text
 from textual.containers import Vertical
 from textual.widget import Widget
 from textual.widgets import Tree
+from textual.widgets.tree import TreeNode
 
 from iolanta.facets.facet import Facet
 from iolanta.facets.page_title import PageTitle
+from iolanta.iolanta import Iolanta
 from iolanta.models import Triple
+from iolanta.namespaces import DATATYPES
+
+
+@dataclass
+class TreeNodeData:
+    """Data attached to a tree node."""
+
+    node: Node
+    is_rendered: bool = False
 
 
 class TriplesTree(Tree):
     """Triples as tree."""
 
-    def __init__(self, triples: list[Triple]):   # noqa: WPS210
+    def __init__(  # noqa: WPS210
+        self,
+        triples: list[Triple],
+        iolanta: Iolanta,
+    ):   # noqa: WPS210
         """Initialize the tree."""
         self.triples = triples
+        self.iolanta = iolanta
         super().__init__(label='Triples')
 
         self.show_root = False
         triples_tree = self._construct_triples_tree(triples)
 
         for subject, properties in triples_tree.items():
-            subject_node = self.root.add(subject, data=subject, expand=False)
+            subject_node = self.root.add(
+                self._format_not_rendered_iri(subject),
+                data=TreeNodeData(subject),
+                expand=False,
+            )
 
             for predicate, obj_nodes in properties.items():
-                predicate_node = subject_node.add(predicate, expand=True)
+                predicate_node = subject_node.add(
+                    self._format_not_rendered_iri(predicate),
+                    data=TreeNodeData(predicate),
+                    expand=True,
+                )
 
                 for obj_node in obj_nodes:
-                    predicate_node.add_leaf(obj_node)
+                    predicate_node.add_leaf(
+                        self._format_not_rendered_iri(obj_node),
+                        data=TreeNodeData(obj_node),
+                    )
+
+    def on_mount(self):
+        """Trigger rendering of titles asynchronously."""
+        self.run_worker(
+            functools.partial(
+                self.render_titles_for_children,
+                self.root,
+            ),
+            thread=True,
+        )
+
+    def render_titles_for_children(
+        self,
+        node: TreeNode[TreeNodeData],
+        recursive: bool = False,
+    ):
+        """Render titles for children."""
+        for child in node.children:
+            if child.data.is_rendered:
+                continue
+
+            iri = child.data.node
+            if isinstance(iri, Literal):
+                label = self._format_not_rendered_iri(iri)
+            else:
+                label = self.iolanta.render(
+                    iri,
+                    as_datatype=DATATYPES.title,
+                )[0]
+
+            self.app.call_from_thread(
+                child.set_label,
+                label,
+            )
+
+            child.data.is_rendered = True
+
+            if recursive:
+                self.render_titles_for_children(child, recursive=True)
+
+    def on_tree_node_expanded(self, event: Tree.NodeExpanded[Node]):
+        """Render titles of children when node is expanded."""
+        self.run_worker(
+            functools.partial(
+                self.render_titles_for_children,
+                event.node,
+                recursive=True,
+            ),
+            thread=True,
+        )
+
+    def _format_not_rendered_iri(self, node: Node):
+        return Text(str(node), style='#696969')
 
     def _construct_triples_tree(self, triples):
         triples_tree = defaultdict(functools.partial(defaultdict, list))
@@ -47,7 +130,7 @@ class GraphFacet(Facet[Widget]):
             for triple in self.stored_query('triples.sparql', graph=self.iri)
         ]
 
-        tree = TriplesTree(triples)
+        tree = TriplesTree(triples=triples, iolanta=self.iolanta)
         triple_count = len(triples)
         return Vertical(
             PageTitle(self.iri, extra=f'({triple_count} triples)'),
