6fd226276e3c47f3aa05af92389a706c8ce02558
Anatoly Scherbakov <altaisoft@gmail.com>
2025-08-08 00:06:20 +0400

    #312 Test the lexvo redirect

--- tests/test_apply_redirect.py
+++ tests/test_apply_redirect.py
@@ -7,3 +7,10 @@ def test_apply_redirect():
     assert apply_redirect(
         URIRef('https://nanopub.org/nschema'),
     ) == URIRef('https://nanopub.net/nschema#')
+
+
+def test_apply_redirect_lexvo():
+    """Test that lexvo.org/id URLs are redirected to lexvo.org/data."""
+    assert apply_redirect(
+        URIRef('http://lexvo.org/id/term/hye/%D5%B6%D6%80%D5%A1%D5%B6%D6%84'),
+    ) == URIRef('http://lexvo.org/data/term/hye/%D5%B6%D6%80%D5%A1%D5%B6%D6%84')
