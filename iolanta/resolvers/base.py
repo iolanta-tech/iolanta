48bd6ccbded1c318c7f4d839a2db1c96cf80ed5a
Anatoly Scherbakov <altaisoft@gmail.com>
2025-08-10 16:20:48 +0400

    #178 `Resolver.__getitem__` → `….resolve()`

--- iolanta/resolvers/base.py
+++ iolanta/resolvers/base.py
@@ -8,6 +8,7 @@ from iolanta.facets.facet import Facet
 
 class Resolver:
     """Resolve facet IRIs into classes."""
+
     @abstractmethod
-    def __getitem__(self, facet_iri: URIRef) -> Type[Facet]:
+    def resolve(self, uri: URIRef) -> Type[Facet]:
         """Find a resolver by IRI."""
