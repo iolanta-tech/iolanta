7bfcfe675d7a9671aaa85a5c8ae056e74a667b9e
Anatoly Scherbakov <altaisoft@gmail.com>
2026-03-22 23:49:56 +0400

    Collapse Wikidata host/scheme redirects into one `http://www.wikidata.org` rule

--- iolanta/sparqlspace/redirects.py
+++ iolanta/sparqlspace/redirects.py
@@ -26,8 +26,8 @@ REDIRECTS = MappingProxyType(
         r"http://lexvo\.org/id/(.+)": r"http://lexvo.org/data/\1",
         r"https://lexvo\.org/id/(.+)": r"http://lexvo.org/data/\1",
         r"https://www\.lexinfo\.net/(.+)": r"http://www.lexinfo.net/\1",
-        # Convert Wikidata https:// to http:// (Wikidata JSON-LD uses http:// URIs)
-        r"https://www\.wikidata\.org/entity/(.+)": r"http://www.wikidata.org/entity/\1",
+        # Wikidata: map bare or www host + http(s) to http://www.wikidata.org/...
+        r"https?://(www\.)?wikidata\.org/(.+)": r"http://www.wikidata.org/\2",
     }
 )
 
