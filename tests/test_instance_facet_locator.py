9c837d024b434dd4adfc4d514da7c143bef63395
Anatoly Scherbakov <altaisoft@gmail.com>
2026-03-28 20:34:15 +0400

    Wrap long test signature and use double quotes for example URI

--- tests/test_instance_facet_locator.py
+++ tests/test_instance_facet_locator.py
@@ -8,14 +8,16 @@ from iolanta.iolanta import Iolanta
 from iolanta.namespaces import IOLANTA
 
 
-def test_by_instance_facet_does_not_crash_with_blank_node_class(iolanta: Iolanta) -> None:
+def test_by_instance_facet_does_not_crash_with_blank_node_class(
+    iolanta: Iolanta,
+) -> None:
     """
     Merged `by_instance_facet` query must not interpolate BNodes as `<_:…>` IRIs.
 
     That used to make the SPARQL processor preload skolem URIs and raise, and
     could recurse badly during exception logging.
     """
-    subject = URIRef('https://example.test/ns#subject')
+    subject = URIRef("https://example.test/ns#subject")
     anonymous_class = BNode()
     iolanta.graph.add((subject, RDF.type, anonymous_class))
     iolanta.graph.add((subject, RDF.type, RDFS.Resource))
