21a5e3f939ea484fdb773479cd7f8dafd91f78bd
Anatoly Scherbakov <altaisoft@gmail.com>
2026-04-26 18:54:48 +0400

    Fix graph triple test lint

--- tests/facets/test_textual_graph_triples.py
+++ tests/facets/test_textual_graph_triples.py
@@ -10,42 +10,48 @@ from iolanta.models import Triple
 
 
 def test_redundant_literal_title_triple_is_hidden():
-    subject = URIRef('https://example.com/UTF-8')
+    subject = URIRef("https://example.com/UTF-8")  # noqa: WPS226
     triple = Triple(
         subject=subject,
         predicate=RDFS.label,
-        object=Literal('UTF-8'),
+        object=Literal("UTF-8"),  # noqa: WPS226
     )
 
     assert not is_informative_triple(
         triple=triple,
-        render_title=cast(Callable[[Node], object], {
-            subject: 'UTF-8',
-            Literal('UTF-8'): 'UTF-8',
-        }.__getitem__),
+        render_title=cast(
+            Callable[[Node], object],
+            {
+                subject: "UTF-8",
+                Literal("UTF-8"): "UTF-8",
+            }.__getitem__,
+        ),
     )
 
 
 def test_different_literal_title_triple_is_kept():
-    subject = URIRef('https://example.com/UTF-8')
+    subject = URIRef("https://example.com/UTF-8")
     triple = Triple(
         subject=subject,
         predicate=RDFS.label,
-        object=Literal('Unicode UTF-8'),
+        object=Literal("Unicode UTF-8"),
     )
 
     assert is_informative_triple(
         triple=triple,
-        render_title=cast(Callable[[Node], object], {
-            subject: 'UTF-8',
-            Literal('Unicode UTF-8'): 'Unicode UTF-8',
-        }.__getitem__),
+        render_title=cast(
+            Callable[[Node], object],
+            {
+                subject: "UTF-8",
+                Literal("Unicode UTF-8"): "Unicode UTF-8",
+            }.__getitem__,
+        ),
     )
 
 
-def test_iri_object_triple_is_kept_when_title_matches():
-    subject = URIRef('https://example.com/UTF-8')
-    object_ = URIRef('https://example.com/encoding/UTF-8')
+def test_iri_object_title_match_is_kept():
+    subject = URIRef("https://example.com/UTF-8")  # noqa: WPS226
+    object_ = URIRef("https://example.com/encoding/UTF-8")
     triple = Triple(
         subject=subject,
         predicate=RDFS.seeAlso,
@@ -54,25 +60,31 @@ def test_iri_object_triple_is_kept_when_title_matches():
 
     assert is_informative_triple(
         triple=triple,
-        render_title=cast(Callable[[Node], object], {
-            subject: 'UTF-8',
-            object_: 'UTF-8',
-        }.__getitem__),
+        render_title=cast(
+            Callable[[Node], object],
+            {
+                subject: "UTF-8",
+                object_: "UTF-8",
+            }.__getitem__,
+        ),
     )
 
 
-def test_non_label_redundant_literal_title_triple_is_hidden():
-    subject = URIRef('https://example.com/UTF-8')
+def test_non_label_duplicate_literal_is_hidden():
+    subject = URIRef("https://example.com/UTF-8")  # noqa: WPS226
     triple = Triple(
         subject=subject,
         predicate=RDFS.comment,
-        object=Literal('UTF-8'),
+        object=Literal("UTF-8"),  # noqa: WPS226
     )
 
     assert not is_informative_triple(
         triple=triple,
-        render_title=cast(Callable[[Node], object], {
-            subject: 'UTF-8',
-            Literal('UTF-8'): 'UTF-8',
-        }.__getitem__),
+        render_title=cast(
+            Callable[[Node], object],
+            {
+                subject: "UTF-8",
+                Literal("UTF-8"): "UTF-8",
+            }.__getitem__,
+        ),
     )
