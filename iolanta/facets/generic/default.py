95b836ca415864b8449ecca807fc4febda1d0087
Anatoly Scherbakov <altaisoft@gmail.com>
2026-06-14 01:53:35 +0400

    Render `iolanta:icon` in generic labels

--- iolanta/facets/generic/default.py
+++ iolanta/facets/generic/default.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 from dataclasses import dataclass
 from functools import cached_property
 from pathlib import Path
-from typing import Optional, TypedDict
+from typing import Optional, cast
 
 import funcy
 from rdflib import Literal
@@ -15,19 +15,19 @@ class Description:
     """A few properties of the object we use as row heading."""
 
     label: Optional[Literal] = None
-    symbol: Optional[Literal] = None
+    icon: Optional[Literal] = None
     url: Optional[Literal] = None
     comment: Optional[Literal] = None
 
 
 class DefaultMixin(Facet[FacetOutput]):
-    stored_queries_path = Path(__file__).parent / 'sparql'
+    stored_queries_path = Path(__file__).parent / "sparql"
 
     @cached_property
     def description(self) -> Description:
         return Description(
             **funcy.first(
-                self.stored_query('default.sparql', iri=self.this),
+                self.stored_query("default.sparql", iri=self.this),
             ),
         )
 
@@ -36,38 +36,45 @@ class DefaultMixin(Facet[FacetOutput]):
         if self.description.url:
             return self.render_link()
 
-        return self.render_label()
+        return cast(FacetOutput, self.render_label())
 
     @abstractmethod
     def render_link(self) -> FacetOutput:
         """Render clickable link."""
 
     def render_label(self) -> str:
-        if not (label := self.description.label):
-            label = self.render_fallback()
+        label = self.description.label
+        rendered_label = str(label.value) if label else self.render_fallback()
 
-        if isinstance(label, Literal):
-            label = label.value
+        if icon := self.description.icon:
+            if self.as_datatype:
+                rendered_icon = self.render(
+                    icon,
+                    as_datatype=self.as_datatype,
+                )
+            else:
+                rendered_icon = icon.value
+            rendered_label = f"{rendered_icon} {rendered_label}"
 
-        if symbol := self.description.symbol:
-            rendered_symbol = self.render(
-                symbol,
-                as_datatype=self.as_datatype,
-            )
-            label = f'{rendered_symbol} {label}'
-
-        return label
+        return rendered_label
 
     def render_fallback(self) -> str:
         string_iri = str(self.this)
 
-        if string_iri.startswith('local:'):
-            string_iri = string_iri.removeprefix(
-                'local:',
-            ).replace(
-                '_', ' ',
-            ).replace(
-                '-', ' ',
-            ).capitalize()
+        if string_iri.startswith("local:"):
+            string_iri = (
+                string_iri.removeprefix(
+                    "local:",
+                )
+                .replace(
+                    "_",
+                    " ",
+                )
+                .replace(
+                    "-",
+                    " ",
+                )
+                .capitalize()
+            )
 
         return string_iri
